78487979b81583464e5f20ef47dca060c4e5b2eb
a7subram <a7subram@uwaterloo.ca>
2020-04-23 13:55:28 -0400

    Refactored

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -2,57 +2,72 @@ import sys
 import re
 import numpy
 
-# This function checks for the intersection point(s) between two lines
+# This function checks whether intersection point(s) exists between two lines
 def checks(x1,y1,x2,y2,x3,y3,x4,y4):
     try:
-        t = ( ((x1-x3)*(y3-y4)) - ((y1-y3)*(x3-x4)) )/( ((x1-x2)*(y3-y4)) - ((y1-y2)*(x3-x4)) )
-        u = -( ((x1-x2)*(y1-y3)) - ((y1-y2)*(x1-x3)) )/( ((x1-x2)*(y3-y4)) - ((y1-y2)*(x3-x4)) )
+        # t and u are part of the line intersection equation
+        t_numerator = ( ((x1-x3)*(y3-y4)) - ((y1-y3)*(x3-x4)) )
+        t_denominator = ( ((x1-x2)*(y3-y4)) - ((y1-y2)*(x3-x4)) )
 
-        if (u < 0 or u > 1 or t < 0 or t > 1):
+        u_numerator = ( ((x1-x2)*(y1-y3)) - ((y1-y2)*(x1-x3)) )
+        u_denominator = ( ((x1-x2)*(y3-y4)) - ((y1-y2)*(x3-x4)) )
+
+        t = t_numerator/t_denominator
+        u = -u_numerator/u_denominator
+
+        #u and t are out of bounds --> distinct lines, not overlapping
+        if (u < 0 or u > 1 or t < 0 or t > 1): 
             return 0
 
-        # Executes if only one intersection point between two lines exist
+        # Executes if only one intersection point exists between two lines 
         if ((0 <= u <= 1) and (0 <= t <= 1)):
             x_coord = x1 + (t * (x2 - x1))
             y_coord = y1 + (t * (y2 - y1))
             return [(float("{0:.2f}".format(x_coord)),float("{0:.2f}".format(y_coord)))]
+    
     except:
         # These statements describe overlapping line segment scenarios for all orientations and returns both intersection points
-        if ((x3 <= x1 <= x4 and x3 <= x2 <= x4 and y3 <= y1 <= y4 and y3 <= y2 <= y4) or
-            (x4 <= x1 <= x3 and x4 <= x2 <= x3 and y4 <= y1 <= y3 and y4 <= y2 <= y3) or
-            (x3 <= x1 <= x4 and x3 <= x2 <= x4 and y3 >= y1 >= y4 and y3 >= y2 >= y4) or
-            (x4 <= x1 <= x3 and x4 <= x2 <= x3 and y4 >= y1 >= y3 and y4 >= y2 >= y3)):
-            return [(x1, y1), (x2, y2)]
+        if (t_numerator == 0 and t_denominator == 0 and u_numerator == 0 and u_denominator == 0): 
+            if ((x3 <= x1 <= x4 and x3 <= x2 <= x4 and y3 <= y1 <= y4 and y3 <= y2 <= y4) or
+                (x4 <= x1 <= x3 and x4 <= x2 <= x3 and y4 <= y1 <= y3 and y4 <= y2 <= y3) or
+                (x3 <= x1 <= x4 and x3 <= x2 <= x4 and y3 >= y1 >= y4 and y3 >= y2 >= y4) or
+                (x4 <= x1 <= x3 and x4 <= x2 <= x3 and y4 >= y1 >= y3 and y4 >= y2 >= y3)):
+                return [(x1, y1), (x2, y2)]
 
-        if ((x2 <= x3 <= x1 and x2 <= x4 <= x1 and y2 <= y3 <= y1 and y2 <= y4 <= y1) or
-            (x1 <= x3 <= x2 and x1 <= x4 <= x2 and y1 <= y3 <= y2 and y1 <= y4 <= y2) or
-            (x2 <= x3 <= x1 and x2 <= x4 <= x1 and y2 >= y3 >= y1 and y2 >= y4 >= y1) or
-            (x1 <= x3 <= x2 and x1 <= x4 <= x2 and y1 >= y3 >= y2 and y1 >= y4 >= y2)):
-            return [(x3, y3), (x4, y4)]
+            if ((x2 <= x3 <= x1 and x2 <= x4 <= x1 and y2 <= y3 <= y1 and y2 <= y4 <= y1) or
+                (x1 <= x3 <= x2 and x1 <= x4 <= x2 and y1 <= y3 <= y2 and y1 <= y4 <= y2) or
+                (x2 <= x3 <= x1 and x2 <= x4 <= x1 and y2 >= y3 >= y1 and y2 >= y4 >= y1) or
+                (x1 <= x3 <= x2 and x1 <= x4 <= x2 and y1 >= y3 >= y2 and y1 >= y4 >= y2)):
+                return [(x3, y3), (x4, y4)]
 
-        if ((x1 <= x3 <= x2 <= x4 and y1 >= y3 >= y2 >= y4) or
-            (x4 <= x2 <= x3 <= x1 and y4 >= y2 >= y3 >= y1) or
-            (x1 >= x3 >= x2 >= x4 and y1 >= y3 >= y2 >= y4) or
-            (x4 >= x2 >= x3 >= x1 and y4 >= y2 >= y3 >= y1)):
-            return [(x3, y3), (x2, y2)]
+            if ((x1 <= x3 <= x2 <= x4 and y1 >= y3 >= y2 >= y4) or
+                (x4 <= x2 <= x3 <= x1 and y4 >= y2 >= y3 >= y1) or
+                (x1 >= x3 >= x2 >= x4 and y1 >= y3 >= y2 >= y4) or
+                (x4 >= x2 >= x3 >= x1 and y4 >= y2 >= y3 >= y1)):
+                return [(x3, y3), (x2, y2)]
 
-        if ((x1 <= x4 <= x2 <= x3 and y1 >= y4 >= y2 >= y3) or
-            (x3 <= x2 <= x4 <= x1 and y3 >= y2 >= y4 >= y1) or
-            (x1 >= x4 >= x2 >= x3 and y1 >= y4 >= y2 >= y3) or
-            (x3 >= x2 >= x4 >= x1 and y3 >= y2 >= y4 >= y1)):
-            return [(x2, y2), (x4, y4)]
-        
-        if ((x2 <= x4 <= x1 <= x3 and y2 >= y4 >= y1 >= y3) or
-            (x3 <= x1 <= x4 <= x2 and y3 >= y1 >= y4 >= y2) or
-            (x2 >= x4 >= x1 >= x3 and y2 >= y4 >= y1 >= y3) or
-            (x3 >= x1 >= x4 >= x2 and y3 >= y1 >= y4 >= y2)):
-            return [(x1, y1), (x4, y4)]
+            if ((x1 <= x4 <= x2 <= x3 and y1 >= y4 >= y2 >= y3) or
+                (x3 <= x2 <= x4 <= x1 and y3 >= y2 >= y4 >= y1) or
+                (x1 >= x4 >= x2 >= x3 and y1 >= y4 >= y2 >= y3) or
+                (x3 >= x2 >= x4 >= x1 and y3 >= y2 >= y4 >= y1)):
+                return [(x2, y2), (x4, y4)]
+            
+            if ((x2 <= x4 <= x1 <= x3 and y2 >= y4 >= y1 >= y3) or
+                (x3 <= x1 <= x4 <= x2 and y3 >= y1 >= y4 >= y2) or
+                (x2 >= x4 >= x1 >= x3 and y2 >= y4 >= y1 >= y3) or
+                (x3 >= x1 >= x4 >= x2 and y3 >= y1 >= y4 >= y2)):
+                return [(x1, y1), (x4, y4)]
 
-        if ((x4 <= x1 <= x3 <= x2 and y4 >= y1 >= y3 >= y2) or
-            (x2 <= x3 <= x1 <= x4 and y2 >= y3 >= y1 >= y4) or
-            (x4 >= x1 >= x3 >= x2 and y4 >= y1 >= y3 >= y2) or
-            (x2 >= x3 >= x1 >= x4 and y2 >= y3 >= y1 >= y4)):
-            return [(x1, y1), (x3, y3)]
+            if ((x4 <= x1 <= x3 <= x2 and y4 >= y1 >= y3 >= y2) or
+                (x2 <= x3 <= x1 <= x4 and y2 >= y3 >= y1 >= y4) or
+                (x4 >= x1 >= x3 >= x2 and y4 >= y1 >= y3 >= y2) or
+                (x2 >= x3 >= x1 >= x4 and y2 >= y3 >= y1 >= y4)):
+                return [(x1, y1), (x3, y3)]
+
+        # The lines are not overlapping --> parallel and not touching, no intersection
+        if (t_denominator == 0 and u_denominator == 0):  
+            return 0
+
     
     return 0
 
@@ -89,7 +104,7 @@ def main():
 
         # a = adding a new street
         if a.fullmatch(prompt):
-            # Next 2 lines format the street name to lowercase and strips all white space in coordinates
+            # Next 2 lines format the street name and coordinates appropriately 
             street_key = street.group().lower() 
             street_value = coord.group().replace(" ", "").replace(")", ".0)").replace(",", ".0,").replace(")(", ") (").split(" ")
 
@@ -140,19 +155,24 @@ def main():
                 for key,value in street_info.items():
                     converted[str(key)] = [eval(item) for item in value]
 
-                # Stores all vertices and edges in these two arrays
+                # Stores all the vertices in the street map
                 vertices = []
-                edges = []
 
-                # This block of code goes through all line segments for every street in the dictionary and evaluates them with each other to determine all intersection points in the street map
+                # Stores unsorted, redundant edge list here --> will be refined later
+                temp_edge_list = []
+
+                # This loop (lines 148-231) finds all valid intersection points in the street map and orders them with respect to distance
                 for key_temp in converted:
                     
-                    # Stores all intersection points in the street map 
+                    # Stores all intersection points in the street map as keys, and stores all line segment endpoints which make these intersection as values
                     ip_dict = {}
 
+                    # Stores the coordinates which make up a street as an array of tuples
                     value_curr = converted[key_temp]
 
+                    # This loop (lines 156-184) finds all intersection points within the street map and stores it into "ip_dict" dictionary
                     for i in range(0, len(value_curr) - 1):
+                        # Extract coordinates for first line segment 
                         x1, y1 = value_curr[i][0], value_curr[i][1]
                         x2, y2 = value_curr[i + 1][0], value_curr[i + 1][1]
                         
@@ -162,9 +182,11 @@ def main():
                                 other_street_val = converted[key]
 
                                 for k in range(0, len(other_street_val) - 1):
+                                    # Extract coordinates for second line segment 
                                     x3, y3 = other_street_val[k][0], other_street_val[k][1]
                                     x4, y4 = other_street_val[k + 1][0], other_street_val[k + 1][1]
 
+                                    # Function "checks" evaluates whether intersection point(s) exist between both line segments in comparison 
                                     ip = checks(x1,y1,x2,y2,x3,y3,x4,y4)
 
                                     if ip != 0:
@@ -178,6 +200,8 @@ def main():
                                             else:
                                                 ip_dict[pt] = [(x1, y1),(x2, y2),(x3, y3),(x4, y4)]
 
+                    
+                    # This loop (lines 188-231) sorts all the intersection points in order by distance - thereby producing a correct edge list for the undirected graph - and creates the edge list
                     for x in range(0, len(value_curr) - 1):
                         
                         temp = []
@@ -196,7 +220,6 @@ def main():
                                 
                                 if p_x1 == p1 and p_y1 == q1 and p_x2 == p2 and p_y2 == q2:
                                     if key_ip not in temp:
-                                       
                                         temp.append(key_ip) 
                                     break
                                 j = j + 1
@@ -220,30 +243,33 @@ def main():
                                 final_list.append(intersection)
                        
                         if len(final_list) >= 2:
-                            
                             for j in range(0,len(final_list)-1):
-                                edges.append((final_list[j], final_list[j+1]))
+                                temp_edge_list.append((final_list[j], final_list[j+1]))
                                 
-                final_edge = []        
-                for item in edges:
-                    if item not in final_edge:
-                        final_edge.append(item)
+                redundant_edge_list = []        
+                for item in temp_edge_list:
+                    if item not in redundant_edge_list:
+                        redundant_edge_list.append(item)
 
-                f_edge = []
-                for y in range(0, len(final_edge)):
-                    duplicate_1 = (final_edge[y][0], final_edge[y][1])
-                    duplicate_2 = (final_edge[y][1], final_edge[y][0])
-                    if (duplicate_1 not in f_edge) and (duplicate_2 not in f_edge):
-                        f_edge.append(duplicate_1)
+                # Checks for and removes any duplicate edges from the edge list
+                final_edge_list = []
+                for y in range(0, len(redundant_edge_list)):
+                    duplicate_1 = (redundant_edge_list[y][0], redundant_edge_list[y][1])
+                    duplicate_2 = (redundant_edge_list[y][1], redundant_edge_list[y][0])
+                    if (duplicate_1 not in final_edge_list) and (duplicate_2 not in final_edge_list):
+                        final_edge_list.append(duplicate_1)
                 
-                for x in range(0, len(f_edge)):
+                # Now that the final edge list is complete, this portion creates the vertice list from the edge list
+                for x in range(0, len(final_edge_list)):
 
-                    if f_edge[x][0] not in vertices:
-                        vertices.append(f_edge[x][0])
+                    if final_edge_list[x][0] not in vertices:
+                        vertices.append(final_edge_list[x][0])
 
-                    if f_edge[x][1] not in vertices:
-                        vertices.append(f_edge[x][1])
+                    if final_edge_list[x][1] not in vertices:
+                        vertices.append(final_edge_list[x][1])
 
+
+                # Prints out the undirected graphs vertice and edge list!
                 if len(vertices) == 0:
                     print("V = {")
                     print("}")
@@ -255,26 +281,26 @@ def main():
                         i += 1
                     print("}")
 
-                if len(f_edge) == 0:
+                if len(final_edge_list) == 0:
                     print("E = {")
                     print("}")
                     
-                elif len(f_edge) == 1:
+                elif len(final_edge_list) == 1:
                     print("E = {")
-                    x = vertices.index(f_edge[0][0])+1
-                    y = vertices.index(f_edge[0][1])+1
+                    x = vertices.index(final_edge_list[0][0])+1
+                    y = vertices.index(final_edge_list[0][1])+1
                     print("\t<",x,",",y,">", sep = "")
                     print("}")
                     
                 else:
                     print("E = {")
-                    for j in range(0, len(f_edge)-1):
-                        x = vertices.index(f_edge[j][0])+1
-                        y = vertices.index(f_edge[j][1])+1
+                    for j in range(0, len(final_edge_list)-1):
+                        x = vertices.index(final_edge_list[j][0])+1
+                        y = vertices.index(final_edge_list[j][1])+1
                         print("\t<",x,",",y,">,", sep = "")
                     j += 1
-                    x = vertices.index(f_edge[j][0])+1
-                    y = vertices.index(f_edge[j][1])+1
+                    x = vertices.index(final_edge_list[j][0])+1
+                    y = vertices.index(final_edge_list[j][1])+1
                     print("\t<",x,",",y,">", sep = "")
                     print("}")
         else:
